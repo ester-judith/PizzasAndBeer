09f4bd78081abe1ba2e259a422cddad92cd7dc15
ester-judith <ester.j.ejcc@gmail.com>
2024-11-06 13:30:40 -0700

    Octavo commit

--- MenuAdminScreen.js
+++ MenuAdminScreen.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView, Image, TextInput, TouchableOpacity, ImageBackground, Modal } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image, TextInput, TouchableOpacity, ImageBackground, Modal, Alert, ActivityIndicator } from 'react-native';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+import * as Print from 'expo-print';
+import * as Sharing from 'expo-sharing';
+import firebaseConfig from './firebase';
 
-const MenuScreen = ({ navigation, route }) => {
+const MenuAdminScreen = ({ navigation, route }) => {
   const { userId } = route.params;
   const [modalVisible, setModalVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   SplashScreen.preventAutoHideAsync();
 
@@ -31,12 +35,112 @@ const MenuScreen = ({ navigation, route }) => {
     navigation.navigate('Login');
   };
 
+  const branchDetails = [
+    { id: 'siete', name: 'Sucursal 7', image: require('./assets/images/sucursal7.jpg') },
+    { id: 'sexta', name: 'Sucursal 6', image: require('./assets/images/sucursal6.jpg') },
+    { id: 'veinte', name: 'Sucursal 20', image: require('./assets/images/sucursal20.jpg') },
+    { id: 'herradura', name: 'Sucursal 26', image: require('./assets/images/sucursal26.jpg') },
+    { id: 'monterrey', name: 'Sucursal Monterrey', image: require('./assets/images/sucursalmty.jpg') },
+    { id: 'mexicali', name: 'Sucursal Mexicali', image: require('./assets/images/sucursalmxl.jpg') },
+  ];
+
+  const fetchProductsForBranch = async (branchId) => {
+    setLoading(true);
+    console.log(`Fetching products for branch: ${branchId}`);
+    try {
+      const querySnapshot = await firebaseConfig.db
+        .collection('user')
+        .doc(branchId)
+        .collection('productos')
+        .get();
+
+      if (querySnapshot.empty) {
+        console.log(`No products found for branch: ${branchId}`);
+        return [];
+      }
+
+      const productsData = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      console.log('Products fetched:', productsData);
+
+      // Filtra productos con stock bajo
+      const lowStockProducts = productsData.filter(
+        product => product.stock < product.stockD
+      );
+
+      console.log('Low stock products:', lowStockProducts);
+
+      return lowStockProducts;
+    } catch (error) {
+      console.log("Error al cargar productos:", error);
+      Alert.alert("Error", "No se pudieron cargar los productos de la sucursal.");
+      return [];
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const generatePDF = async (branchId) => {
+    if (!branchId) {
+      console.log("No branch ID provided");
+      return;
+    }
+
+    Alert.alert(
+      'Confirmación',
+      `¿Estás seguro que quieres generar el reporte de inventario para la ${branchId}?`,
+      [
+        { text: 'Cancelar', onPress: () => {}, style: 'cancel' },
+        {
+          text: 'Generar PDF', onPress: async () => {
+            const lowStockProducts = await fetchProductsForBranch(branchId);
+
+            // Verifica si hay productos con stock bajo antes de generar el PDF
+            if (lowStockProducts.length === 0) {
+              Alert.alert("Inventario en orden", "No hay productos con stock bajo.");
+              return;
+            }
+
+            // Construye el HTML para el PDF
+            const htmlContent = `
+              <h1>Reporte de Inventario - Sucursal ${branchId}</h1>
+              <p>Productos con stock bajo:</p>
+              <ul>
+                ${lowStockProducts.map(product => `
+                  <li>${product.nombreProducto}: ${product.stock} / ${product.stockD}</li>
+                `).join('')}
+              </ul>
+            `;
+            console.log("HTML Content:", htmlContent);
+
+            try {
+              // Genera el PDF con el contenido HTML usando expo-print
+              const { uri } = await Print.printToFileAsync({ html: htmlContent });
+
+              console.log("PDF generado en:", uri);
+
+              // Usar Expo Sharing para permitir que el usuario descargue el archivo
+              await Sharing.shareAsync(uri);
+              Alert.alert("PDF Generado", "El PDF ha sido generado y está listo para compartirse.");
+
+            } catch (error) {
+              console.log("Error al generar el PDF:", error);
+              Alert.alert("Error", "No se pudo generar el PDF.");
+            }
+          }
+        }
+      ]
+    );
+  };
+
   return (
     <ImageBackground
       source={require('./assets/images/backgroundtres.png')}
       style={styles.background}
     >
-      {/* Contenido Fijo */}
       <View style={styles.headerContainer}>
         <TouchableOpacity>
           <Image
@@ -63,58 +167,33 @@ const MenuScreen = ({ navigation, route }) => {
 
       <View style={styles.imageFrame}>
         <Image
-          source={require('./assets/images/comida.jpg')} 
+          source={require('./assets/images/comida.jpg')}
           style={styles.image}
         />
       </View>
 
-      {/* ScrollView solo para las categorías */}
       <ScrollView contentContainerStyle={styles.categoriesContainer}>
-        <TouchableOpacity style={styles.categoryBox}>
-          <Image
-            source={require('./assets/images/sucursal7.jpg')}
-            style={styles.categoryImage}
-          />
-          <Text style={styles.categoryText}>Sucursal 7</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.categoryBox}>
-          <Image
-            source={require('./assets/images/sucursal6.jpg')}
-            style={styles.categoryImage}
-          />
-          <Text style={styles.categoryText}>Sucursal 6</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.categoryBox}>
-          <Image
-            source={require('./assets/images/sucursal20.jpg')}
-            style={styles.categoryImage}
-          />
-          <Text style={styles.categoryText}>Sucursal 20</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.categoryBox}>
-          <Image
-            source={require('./assets/images/sucursal26.jpg')}
-            style={styles.categoryImage}
-          />
-          <Text style={styles.categoryText}>Sucursal 26</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.categoryBox}>
-          <Image
-            source={require('./assets/images/sucursalmty.jpg')}
-            style={styles.categoryImage}
-          />
-          <Text style={styles.categoryText}>Sucursal Monterrey</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.categoryBox}>
-          <Image
-            source={require('./assets/images/sucursalmxl.jpg')}
-            style={styles.categoryImage}
-          />
-          <Text style={styles.categoryText}>Sucursal Mexicali</Text>
-        </TouchableOpacity>
+        {branchDetails.map((branch, index) => (
+          <TouchableOpacity
+            key={index}
+            style={styles.categoryBox}
+            onPress={() => generatePDF(branch.id)}
+          >
+            <Image
+              source={branch.image}
+              style={styles.categoryImage}
+            />
+            <Text style={styles.categoryText}>{branch.name}</Text>
+          </TouchableOpacity>
+        ))}
       </ScrollView>
 
-      {/* Modal */}
+      {loading && (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#ffffff" />
+        </View>
+      )}
+
       <Modal
         animationType="slide"
         transparent={true}
@@ -143,31 +222,31 @@ const styles = StyleSheet.create({
   },
   headerContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between', 
+    justifyContent: 'space-between',
     alignItems: 'center',
     padding: 20,
   },
   icon: {
     width: 60,
     height: 60,
-    marginHorizontal: 8, 
+    marginHorizontal: 8,
   },
   welcomeContainer: {
-    alignItems: 'center', 
-    flex: 1, 
+    alignItems: 'center',
+    flex: 1,
   },
   welcomeText: {
-    color: 'yellow', 
+    color: 'yellow',
     fontSize: 30,
     fontFamily: 'Sanuroemi',
   },
   sucursalText: {
-    color: '#219521', 
+    color: '#219521',
     fontSize: 20,
     fontFamily: 'Rosmatika',
   },
   logoutText: {
-    color: 'white', 
+    color: 'white',
     fontSize: 20,
   },
   searchBar: {
@@ -208,7 +287,7 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 25,
     marginLeft: 20,
-    fontFamily: 'Rosmatika'
+    fontFamily: 'Rosmatika',
   },
   categoryImage: {
     width: 60,
@@ -219,7 +298,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
   modalContent: {
     width: '80%',
@@ -233,16 +312,23 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   logoutButton: {
-    backgroundColor: 'red',
+    backgroundColor: '#ff6347',
     padding: 10,
-    borderRadius: 10,
     marginBottom: 10,
+    borderRadius: 10,
   },
   closeButtonText: {
-    color: 'blue',
+    color: '#ff6347',
     fontSize: 16,
-    marginTop: 10,
+    textDecorationLine: 'underline',
+  },
+  loadingContainer: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: [{ translateX: -50 }, { translateY: -50 }],
+    zIndex: 1,
   },
 });
 
-export default MenuScreen;
+export default MenuAdminScreen;
